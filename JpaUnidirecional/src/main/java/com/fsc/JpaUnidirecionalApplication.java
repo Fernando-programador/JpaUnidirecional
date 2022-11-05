e2fa01f57f77d763f38dc7354df0e68521740de5
Fernando-Programador <suporte@ajeitandoseulado.com.br>
2022-11-05 04:51:38 -0300

    Inicio projeto

---
+++ JpaUnidirecional/src/main/java/com/fsc/JpaUnidirecionalApplication.java
@@ -0,0 +1,13 @@
+package com.fsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaUnidirecionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaUnidirecionalApplication.class, args);
+	}
+
+}
---
+++ JpaUnidirecional/src/test/java/com/fsc/JpaUnidirecionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaUnidirecionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
